5998983d32d46f84470c2646dc02ad12489f63d1
boonwee24 <59058149+boonwee24@users.noreply.github.com>
2021-02-13 16:14:19 +0800

    Committed Unity Red Rooms Update

---
+++ GAM300_LEVEL/Assets/Scripts/GameController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    bool killed; 
+    public GameObject player;
+    public GameObject respawnPoint;
+
+    private void Update()
+    {
+        if (killed == true)
+        {
+            player.transform.position = respawnPoint.transform.position;
+            Debug.Log("TEST");
+            killed = false;
+            Physics.SyncTransforms();
+        }
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            killed = true;
+        }
+    }
+
+}
